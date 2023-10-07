b11d6113dd8c9c7860a974d7ed09af11e78829e2
abrillantes93 <133722284+abrillantes93@users.noreply.github.com>
2023-10-07 14:29:20 -0400

    Add files via upload

---
+++ MilesToKilometersTable.java
@@ -0,0 +1,12 @@
+public class MilesToKilometersTable {
+    public static void main(String[] args){
+        System.out.println("Miles--> Kilometers");
+        for (int i = 0; i <= 10; ++i){
+            double km = i * 1.609;
+            System.out.println(i + "-->"+ km);
+
+
+        }
+
+    }
+}
