1ac3032f0728f74cb8d909ec979af4707bbbd70d
abrillantes93 <133722284+abrillantes93@users.noreply.github.com>
2023-11-16 18:57:02 -0500

    Add files via upload

--- SortNumbers.java
+++ SortNumbers.java
@@ -10,13 +10,28 @@ public class SortNumbers {
         displaySortedNumbers(num1, num2, num3);
 
     }
-    
+    public static void displaySortedNumbers (double num1, double num2, double num3){
+        double[] numbers = { num1, num2, num3};
+        int n = numbers.length;
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    //swap
+                    double holdV = numbers[j];
+                    numbers[j] = numbers[j +1];
+                    numbers[j+1] = holdV;
 
-public static void displaySortedNumbers(double num1, double num2, double num3){
-    double[] numbers ={  num1, num2, num3};
-    Arrays.sort(numbers);
-    System.out.println("Sorted Numbers: " + Arrays.toString(numbers));
-}
+                }
+            }
+        }
+        System.out.println("Sorted Numbers: " + Arrays.toString(numbers));
+    }
+    //.sort function
+    /*public static void displaySortedNumbers(double num1, double num2, double num3){
+        double[] numbers ={  num1, num2, num3};
+        Arrays.sort(numbers);
+        System.out.println("Sorted Numbers: " + Arrays.toString(numbers));
+    } */
 }
 
 
